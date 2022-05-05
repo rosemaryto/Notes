f0852cd83661007314bf7b6a4bca80abeb342c75
rosemaryto <rosemary_to@ymail.com>
2022-05-05 05:46:55 -0700

    new notes

---
+++ notes4-27-22/script4-27-22.js
@@ -0,0 +1,9 @@
+/*WORKING W/ STRINGS
+-literal-constant, fixed sequence of characters between quotation marks
+-primitive-5 data types: string, number, Boolea, undefined, null. Everything else is an object - as soon as you reference a string property/method, the primitive is converted to an object automatically
+object-used to explixitly tell interpreter that you want to create new strong object (not recommended)
+	-supports 3 properts & many methods
+
+    -char data type is one with fixed length
+-trim(): removes white spaces
+*/
